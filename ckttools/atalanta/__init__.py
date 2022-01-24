241d245f96844f8a99b0f528fafa3499cc5835a1
Lucas Nestor <lucas15nestor@gmail.com>
2022-01-24 12:14:27 -0500

    Add atalanta runner

--- ckttools/atalanta/__init__.py
+++ ckttools/atalanta/__init__.py
@@ -1,1 +1,2 @@
 from .fault import create_fault_file
+from .runner import run_atalanta
---
+++ ckttools/atalanta/runner.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def run_atalanta(bench_filename, fault_filename, log_filename):
+    rslt = subprocess.run(["bin/atalanta", "-A", bench_filename, "-f", fault_filename], capture_output=True, text=True)
+
+    # TODO: parse stderr for error
+    with open(log_filename, "w") as f:
+        f.write("%s\n" % rslt.stdout)
+        f.write("----\n")
+        f.write(rslt.stderr)
