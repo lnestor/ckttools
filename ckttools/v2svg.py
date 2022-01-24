d53cd29a946cc1ce8a80558c2cc82836a63eeb0d
Lucas Nestor <lucas15nestor@gmail.com>
2022-01-24 15:38:28 -0500

    Add default output filename to v2svg

--- ckttools/v2svg.py
+++ ckttools/v2svg.py
@@ -6,14 +6,16 @@ import subprocess
 def main():
     parser = argparse.ArgumentParser(description="Convert a verilog file to an image")
     parser.add_argument("verilog_file")
-    parser.add_argument("output_file")
+    parser.add_argument("--output_file", "-o")
     args = parser.parse_args()
 
     ast, _ = parse([args.verilog_file], debug=False)
     module_name = ast.children()[0].children()[0].name
 
+    output_file = args.output_file if args.output_file is not None else "tmp/circuit.svg"
+
     subprocess.run(["yosys", "-p", 'prep -top %s; write_json output.json' % module_name, args.verilog_file])
-    subprocess.run(["netlistsvg", "output.json", "-o", args.output_file])
+    subprocess.run(["netlistsvg", "output.json", "-o", output_file])
     subprocess.run(["rm", "output.json"])
 
 if __name__ == "__main__":
