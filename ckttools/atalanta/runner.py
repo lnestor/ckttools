37a88f446b0d88ebdcf59aa7a44553370fa5b188
Lucas Nestor <lucas15nestor@gmail.com>
2022-01-28 10:05:40 -0500

    Improve testing for errors in Atalanta
    Instead of looking for "Error" in the beginning of the stderr result,
    look for it anywhere in the result. It doesn't necessarily come
    at the beginning.

--- ckttools/atalanta/runner.py
+++ ckttools/atalanta/runner.py
@@ -3,7 +3,7 @@ import subprocess
 def run_atalanta(bench_filename, fault_filename, log_filename, output_filename):
     rslt = subprocess.run(["bin/atalanta", "-A", bench_filename, "-f", fault_filename, "-t", output_filename], capture_output=True, text=True)
 
-    if rslt.stderr.startswith("Error"):
+    if "Error" in rslt.stderr:
         raise RuntimeError(rslt.stderr)
 
     with open(log_filename, "w") as f:
