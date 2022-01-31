f4876b90b2ea86f172be56f88b61f4eb29adcd24
Lucas Nestor <lucas15nestor@gmail.com>
2022-01-31 12:03:53 -0500

    Make atalanta runner tests work
    The tests weren't working because the temporary directory/filenames
    were too long and overflowing a buffer inside atalanta. The
    filenames were being concatenated. To fix this, use flag
    --basedir=tmp/tests to set the temporary directory to a shorter name.

--- tests/atalanta/test_runner.py
+++ tests/atalanta/test_runner.py
@@ -6,32 +6,45 @@ BAD_BENCH = """# example
 
 INPUT(in1)
 INPUT(in2)
+INPUT(in3)
+OUTPUT(out1)
+
+out1 = and(in1, in2)
+"""
+
+GOOD_BENCH = """# example
+#
+
+INPUT(in1)
+INPUT(in2)
 OUTPUT(out1)
 
 out1 = and(in1, in2)
 """
 
 GOOD_FAULT = "out1 /0"
+BAD_FAULT = "out2 /1"
 
-@pytest.mark.skip(reason="Atalanta can't read the files")
-def test_run_atalanta_with_bench_error(tmpdir):
-    # bench_file = tmp_path / "example.bench"
-    # fault_file = tmp_path / "example.flt"
-    # log_file = tmp_path / "example.log"
-    # output_file = tmp_path / "example.test"
+def test_run_atalanta_with_bench_error(tmp_path):
+    bench_file = tmp_path / "example.bench"
+    fault_file = tmp_path / "example.flt"
+    log_file = tmp_path / "example.log"
+    output_file = tmp_path / "example.test"
 
-    # bench_file.write_text(BAD_BENCH)
-    # fault_file.write_text(GOOD_FAULT)
-
-    bench_file = tmpdir.join("example.bench")
-    bench_file.write(BAD_BENCH)
-    fault_file = tmpdir.join("example.flt")
-    log_file = tmpdir.join("example.log")
-    output_file = tmpdir.join("example.test")
+    bench_file.write_text(BAD_BENCH)
+    fault_file.write_text(GOOD_FAULT)
 
     with pytest.raises(RuntimeError):
         run_atalanta(bench_file, fault_file, log_file, output_file)
 
-@pytest.mark.skip(reason="Atalanta can't read the files")
-def test_run_atalanta_with_fault_error():
-    raise
+def test_run_atalanta_with_fault_error(tmp_path):
+    bench_file = tmp_path / "example.bench"
+    fault_file = tmp_path / "example.flt"
+    log_file = tmp_path / "example.log"
+    output_file = tmp_path / "example.test"
+
+    bench_file.write_text(GOOD_BENCH)
+    fault_file.write_text(BAD_FAULT)
+
+    with pytest.raises(RuntimeError):
+        run_atalanta(bench_file, fault_file, log_file, output_file)
