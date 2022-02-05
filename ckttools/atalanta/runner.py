5522dab0e00e36a930e451b05a5900952d44a3e0
Lucas Nestor <lucas15nestor@gmail.com>
2022-02-05 16:08:54 -0500

    Add option to limit how many faults Atalanta finds
    With large circuits, the -A option for Atalanta finds all faults
    and can take awhile. With c1908 (32 inputs), it took like 15ish
    seconds. However, the -D option allows us to input how many faults
    we want to find. This is almost a must use, as the c1908 found
    1.5 million fault strings. Note that that is fault strings, each
    of which have several unknown values.
    
    This change gives an optional number of samples to the atalanta
    runner. If it is present, it uses the -D flag. If not, it uses -A.

--- ckttools/atalanta/runner.py
+++ ckttools/atalanta/runner.py
@@ -1,7 +1,12 @@
 import subprocess
 
-def run_atalanta(bench_filename, fault_filename, log_filename, output_filename):
-    rslt = subprocess.run(["bin/atalanta", "-A", bench_filename, "-f", fault_filename, "-t", output_filename], capture_output=True, text=True)
+def run_atalanta(bench_filename, fault_filename, log_filename, output_filename, num_samples=None):
+    if num_samples is None:
+        args = ["bin/atalanta", "-A", bench_filename, "-f", fault_filename, "-t", output_filename]
+    else:
+        args = ["bin/atalanta", "-D", str(num_samples), bench_filename, "-f", fault_filename, "-t", output_filename]
+
+    rslt = subprocess.run(args, capture_output=True, text=True)
 
     if "error" in rslt.stderr.lower():
         raise RuntimeError(rslt.stderr)
--- ckttools/propagation/input_patterns.py
+++ ckttools/propagation/input_patterns.py
@@ -29,11 +29,11 @@ def switch_to_key_gate_name(input_patterns, key_metadata):
     gate_name_map = {m["output_net"]: gate_name for gate_name, m in key_metadata.items()}
     return {gate_name_map[output]: patterns for output, patterns in input_patterns.items()}
 
-def get_input_patterns(moddef, key_metadata):
+def get_input_patterns(moddef, key_metadata, num_samples=None):
     create_oracle_bench_file(moddef, "tmp/oracle.bench", key_metadata)
     create_fault_file("tmp/oracle.flt", [m["output_net"] for _, m in key_metadata.items()])
 
-    run_atalanta("tmp/oracle.bench", "tmp/oracle.flt", "tmp/oracle.log", "tmp/oracle.test")
+    run_atalanta("tmp/oracle.bench", "tmp/oracle.flt", "tmp/oracle.log", "tmp/oracle.test", num_samples)
     input_patterns = parse_test_file("tmp/oracle.test")
     input_patterns = switch_to_key_gate_name(input_patterns, key_metadata)
 
