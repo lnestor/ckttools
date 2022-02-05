a4021f394246140bcb0925af0bc248557ed8c8b4
Lucas Nestor <lucas15nestor@gmail.com>
2022-02-05 13:52:27 -0500

    Update get input patterns to parse from moddef instead of verilog

--- ckttools/propagation/input_patterns.py
+++ ckttools/propagation/input_patterns.py
@@ -3,15 +3,15 @@ from atalanta import (
     parse_test_file,
     run_atalanta
 )
-from bench.parse import parse_from_verilog
+from bench.parse import parse_from_moddef
 import os
 
 def cleanup(*filenames):
     for filename in filenames:
         os.remove(filename)
 
-def create_oracle_bench_file(verilog_filename, bench_filename, key_metadata):
-    bench = parse_from_verilog(verilog_filename)
+def create_oracle_bench_file(moddef, bench_filename, key_metadata):
+    bench = parse_from_moddef(moddef)
 
     for key_gate_name in key_metadata:
         m = key_metadata[key_gate_name]
@@ -26,8 +26,8 @@ def switch_to_key_gate_name(input_patterns, key_metadata):
     gate_name_map = {m["output_net"]: gate_name for gate_name, m in key_metadata.items()}
     return {gate_name_map[output]: patterns for output, patterns in input_patterns.items()}
 
-def get_input_patterns(filename, key_metadata):
-    create_oracle_bench_file(filename, "tmp/oracle.bench", key_metadata)
+def get_input_patterns(moddef, key_metadata):
+    create_oracle_bench_file(moddef, "tmp/oracle.bench", key_metadata)
     create_fault_file("tmp/oracle.flt", [m["output_net"] for _, m in key_metadata.items()])
 
     run_atalanta("tmp/oracle.bench", "tmp/oracle.flt", "tmp/oracle.log", "tmp/oracle.test")
--- tests/propagation/test_input_patterns.py
+++ tests/propagation/test_input_patterns.py
@@ -1,12 +1,14 @@
 from ckttools.propagation.input_patterns import get_input_patterns
 from ckttools.keygates.metadata import parse_metadata
+from ckttools.vast.search import get_moddef_from_verilog
 from tests.fixtures.fixtures import get_fixture_path
 
 def test_get_input_patterns_simple():
     locked_filename = get_fixture_path("simple_propagation_locked")
+    moddef = get_moddef_from_verilog(locked_filename)
     metadata = parse_metadata(locked_filename)
 
-    input_patterns = get_input_patterns(locked_filename, metadata)
+    input_patterns = get_input_patterns(moddef, metadata)
 
     # Dont current have a way to combine to to 'dont care' values
     assert set(input_patterns["KeyGate1"]) == set(["01", "11"])
@@ -14,27 +16,30 @@ def test_get_input_patterns_simple():
 
 def test_get_input_patterns_branching():
     locked_filename = get_fixture_path("branching_propagation_locked")
+    moddef = get_moddef_from_verilog(locked_filename)
     metadata = parse_metadata(locked_filename)
 
-    input_patterns = get_input_patterns(locked_filename, metadata)
+    input_patterns = get_input_patterns(moddef, metadata)
 
     # Dont current have a way to combine to to 'dont care' values
     assert set(input_patterns["KeyGate2"]) == set(["001", "011", "10x", "11x"])
 
 def test_get_input_patterns_single_branch():
     locked_filename = get_fixture_path("single_branch_propagation_locked")
+    moddef = get_moddef_from_verilog(locked_filename)
     metadata = parse_metadata(locked_filename)
 
-    input_patterns = get_input_patterns(locked_filename, metadata)
+    input_patterns = get_input_patterns(moddef, metadata)
 
     # Dont current have a way to combine to to 'dont care' values
     assert set(input_patterns["KeyGate2"]) == set(["x01", "x11"])
 
 def test_get_input_patterns_input_fault():
     locked_filename = get_fixture_path("input_propagation_locked")
+    moddef = get_moddef_from_verilog(locked_filename)
     metadata = parse_metadata(locked_filename)
 
-    input_patterns = get_input_patterns(locked_filename, metadata)
+    input_patterns = get_input_patterns(moddef, metadata)
 
     # Dont current have a way to combine to to 'dont care' values
     assert set(input_patterns["KeyGate1"]) == set(["11xx", "10xx", "0101", "0001"])
