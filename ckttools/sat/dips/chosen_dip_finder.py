d15d6eda0bf5985a4e470f23a419846d4d31ab31
Lucas Nestor <lucas15nestor@gmail.com>
2022-05-18 15:04:17 -0400

    Add ChosenDipFinder
    This dip finder will take in user provided DIPs are supply
    those to the SAT attack.

--- ckttools/sat/dips/chosen_dip_finder.py
+++ ckttools/sat/dips/chosen_dip_finder.py
@@ -1,3 +1,36 @@
+from util.lazyprop import lazyprop
+
 class ChosenDipFinder:
-    def __init__(self, moddef, args):
-        a = 1
+    def __init__(self, moddef, dip_file):
+        self._moddef = moddef
+        self._dip_file = dip_file
+
+    @lazyprop
+    def _dip_generator(self):
+        with open(self._dip_file) as f:
+            self._n = int(f.readline())
+            lines = f.readlines()
+            inputs = "".join(l.strip().replace(" ", "") for l in lines)
+
+        groups = [inputs[i:i+self._n] for i in range(0, len(inputs), self._n)]
+        return iter(groups)
+
+    def can_find_dip(self):
+        try:
+            self._next = next(self._dip_generator)
+            return True
+        except StopIteration:
+            return False
+
+    def get_dip(self):
+        inputs = self._moddef.primary_inputs[0:self._n]
+        return {input_: val for input_, val in zip(inputs, self._next)}
+
+    def get_keys(self):
+        string = "X" * len(self._moddef.key_inputs)
+        return (string, string)
+
+    def add_constraint(self):
+        return
+
+
---
+++ tests/sat/dips/test_chosen_dip_finder.py
@@ -0,0 +1,69 @@
+from ckttools.sat.dips.chosen_dip_finder import ChosenDipFinder
+import pytest
+
+DIPS = """2
+00
+01
+10
+11
+"""
+
+class DummyModdef():
+    @property
+    def primary_inputs(self):
+        return ["G1", "G2", "G3", "G4"]
+
+    @property
+    def key_inputs(self):
+        return ["K1", "K2", "K3", "K4"]
+
+@pytest.fixture()
+def moddef():
+    return DummyModdef()
+
+def test_can_find_dip_with_dips_remaining(moddef, tmp_path):
+    dip_file = tmp_path / "dips.txt"
+    dip_file.write_text(DIPS)
+
+    dip_finder = ChosenDipFinder(moddef, dip_file)
+    assert dip_finder.can_find_dip()
+
+def test_can_find_dip_with_no_dips_remaining(moddef, tmp_path):
+    dip_file = tmp_path / "dips.txt"
+    dip_file.write_text(DIPS)
+
+    dip_finder = ChosenDipFinder(moddef, dip_file)
+    dip_finder.can_find_dip()
+    dip_finder.can_find_dip()
+    dip_finder.can_find_dip()
+    dip_finder.can_find_dip()
+    assert not dip_finder.can_find_dip()
+
+def test_get_inputs_returns_specified_dips_with_one_liner(moddef, tmp_path):
+    dip_file = tmp_path / "dips.txt"
+    dip_file.write_text(DIPS)
+
+    dip_finder = ChosenDipFinder(moddef, dip_file)
+    dip_finder.can_find_dip()
+    dips = dip_finder.get_dip()
+    assert dips["G1"] == "0"
+    assert dips["G2"] == "0"
+
+    dip_finder.can_find_dip()
+    dips = dip_finder.get_dip()
+    assert dips["G1"] == "0"
+    assert dips["G2"] == "1"
+
+    dip_finder.can_find_dip()
+    dips = dip_finder.get_dip()
+    assert dips["G1"] == "1"
+    assert dips["G2"] == "0"
+
+    dip_finder.can_find_dip()
+    dips = dip_finder.get_dip()
+    assert dips["G1"] == "1"
+    assert dips["G2"] == "1"
+
+def test_get_keys_returns_dummy(moddef):
+    dip_finder = ChosenDipFinder(moddef, "dip_file")
+    assert dip_finder.get_keys() == ("XXXX", "XXXX")
